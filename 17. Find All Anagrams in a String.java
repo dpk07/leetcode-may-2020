c55e25b9c57bae5491b0d1181bc2ab441afcf83e
Deepak Pai <paidpk07@gmail.com>
2020-05-17 14:52:30 +0530

    Updated code for 17th problem

--- 17. Find All Anagrams in a String.java
+++ 17. Find All Anagrams in a String.java
@@ -1,29 +1,32 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        if(p.length()>s.length()){
+        if (p.length() > s.length()) {
             return new ArrayList<Integer>();
         }
         List<Integer> result = new ArrayList<Integer>();
         int[] rescount = new int[26];
-        for(int j=0;j<p.length();j++){
-            rescount[p.charAt(j)-'a']+=1;
+        for (int j = 0; j < p.length(); j++) {
+            rescount[p.charAt(j) - 'a'] += 1;
         }
         int[] count = new int[26];
-        int match=p.length();
-        for(int i=0;i<s.length();i++){
-            if(i>=p.length()){
-                if(count[s.charAt(i-p.length())-'a']>0){
-                    count[s.charAt(i-p.length())-'a']-=1;
-                    match++;
+        for (int i = 0; i < s.length(); i++) {
+            if (i >= p.length()) {
+                if (count[s.charAt(i - p.length()) - 'a'] > 0) {
+                    count[s.charAt(i - p.length()) - 'a'] -= 1;
                 }
             }
-            count[s.charAt(i)-'a']+=1;
-            if(count[s.charAt(i)-'a']==rescount[s.charAt(i)-'a'])match--;
-            if(match==0){
-                result.add(i-p.length()+1);
+            count[s.charAt(i) - 'a'] += 1;
+            int j = 0;
+            for (j = 0; j < p.length(); j++) {
+                // System.out.println(p.charAt(j)+" "+rescount[p.charAt(j)-'a']+"
+                // "+count[p.charAt(j)-'a']);
+                if (rescount[p.charAt(j) - 'a'] != count[p.charAt(j) - 'a'])
+                    break;
+            }
+            if (j == p.length()) {
+                result.add(i - p.length() + 1);
             }
         }
         return result;
     }
 }
-
